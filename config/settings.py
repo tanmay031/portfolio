c2d768cad5b36715232e79ae085cd66b63aeda71
Md Mahbubur Rahman <tanmay100701@gmail.com>
2026-06-07 00:10:10 +0200

    Fix Render allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(k1_r3b93g+chf)u5=i3$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS',
+    '127.0.0.1,localhost,portfolio-tkku.onrender.com,.onrender.com',
+).split(',')
 
 
 # Application definition
