569f533d0f19fac2fb89eee32f6d7532d839f217
Md Mahbubur Rahman <tanmay100701@gmail.com>
2026-06-07 16:14:15 +0200

    Fix ALLOWED_HOSTS for Lightsail deployment

--- config/settings.py
+++ config/settings.py
@@ -26,10 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(k1_r3b93g+chf)u5=i3$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get(
-    'ALLOWED_HOSTS',
-    '127.0.0.1,localhost,portfolio-tkku.onrender.com,.onrender.com',
-).split(',')
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
